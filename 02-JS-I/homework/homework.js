ceb52a5c78ec2285981cf0a1b4d68d1f8f07567a
Edo <edo.katt@gmail.com>
2022-05-26 18:34:18 -0300

    moving on

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -179,27 +179,30 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (numero > 0) return 'Es positivo' ;
+  else if (numero < 0) return 'Es negativo' ;
+  else return false ;
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + '!' ;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return nombre + apellido ;
+  return nombre + ' ' + apellido ;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return 'Hola' + ' ' + nombre + '!' ;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -236,10 +239,17 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
-  //Escribe tu código aquí
-  
+  //Escribe tu código aquí:
+  if (letra.length > 1) {
+    return 'Dato incorrecto' ;
+  }
+  if (letra === 'a' || letra === 'e' || letra === 'i' ||  letra === 'o' || letra === 'u'){
+    return 'Es vocal';
+  }
+  else {
+    return 'Dato incorrecto';
+  }
 }
-
 
 
 // No modificar nada debajo de esta línea

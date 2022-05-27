b5c648c98fed9ec3a2fd7496db9d06d236815603
Edo <edo.katt@gmail.com>
2022-05-26 23:00:49 -0300

    JSI 35/35

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 2;
+const nuevoNum = 3;
 
 // Crea una variable booleana:
 const nuevoBool = true;
